974184460d848ba4c39e23dce5fe8de033afe07c
Rashit Azizbaev <syndicut@gmail.com>
2012-06-27 19:05:54 +0400

    Create credits on every order

--- app/controllers/checkout_controller_decorator.rb
+++ app/controllers/checkout_controller_decorator.rb
@@ -6,11 +6,16 @@ CheckoutController.class_eval do
   def after_complete
     session[:order_id] = nil
 
-    if current_user && current_user.affiliate_partner && current_user.orders.where(:state => 'complete').count == 1
+    if current_user && current_user.affiliate_partner
       sender = current_user.partner
 
       #create credit (if required)
-      create_affiliate_credits(sender, current_user, "purchase")
+      create_affiliate_credits(sender, current_user, first_order?(current_user) ? "first purchase" : "purchase", @order)
     end
   end
+
+  def first_order?(user)
+    user.orders.where(:state => 'complete').count == 1
+  end
+  
 end
