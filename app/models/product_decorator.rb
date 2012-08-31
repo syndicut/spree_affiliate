68581141b35cc38b3f0919c49797011e20d7c609
Rashit Azizbaev <syndicut@gmail.com>
2012-09-01 01:35:08 +0400

    Added setter for bonus_credit

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -1,11 +1,21 @@
 Product.class_eval do
+  attr_accessible :bonus_credit
+
+  def credit_property_id
+    Spree::Config[:recipient_credit_product_property].to_i
   def bonus_credit
-    property_id = Spree::Config[:recipient_credit_product_property].to_i
-    product_property = ProductProperty.find_by_product_id_and_property_id(id, property_id)
+    product_property = ProductProperty.find_by_product_id_and_property_id(id, self.credit_property_id)
     if product_property
       product_property.value.to_f
     else
       0
     end
+  end
+  def bonus_credit=(value)
+    product_property = ProductProperty.find_or_create_by_product_id_and_property_id(
+      id, 
+      self.credit_property_id
+      )
+    product_property.value = value.to_f
   end
 end
